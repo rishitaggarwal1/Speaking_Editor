d6401b6e1eb11d74eea036bf69a84df0c0711183
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-05-08 11:58:01 +0530

    Front-End Changed and Male Female Sound Added

--- speakingApp/views.py
+++ speakingApp/views.py
@@ -4,14 +4,20 @@ import pyttsx3
 
 def home(request):
     if request.method == "POST":
-        text = request.POST['text']
         engine = pyttsx3.init()
         rate = engine.getProperty('rate')
         engine.setProperty('rate', 125)
         volume = engine.getProperty('volume')
         engine.setProperty('volume', 1.0)
         voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[0].id)
-        engine.say(text)
-        engine.runAndWait()
+        s = request.POST['sound']
+        sound = int(s)
+        engine.setProperty('voice', voices[sound].id)
+        if request.POST['text']:
+            text = request.POST['text']
+            engine.say(text)
+            engine.runAndWait()
+        if request.POST['upload']:
+            file = request.POST['upload']
+            print(file)
     return render(request, 'home.html', {})
